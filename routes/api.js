39b43233aedd22925f0d8eed10d4129f4f509597
Kevin Chandler <im.kevin@me.com>
2014-11-02 21:30:27 -0800

    better responses

--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,24 @@ var dotenv = require('dotenv').load();
 var AfkBot = require('../lib/afkbot.js');
 
 router.get('/arm', function(req, res) {
-  if (req.query.passcode && req.query.passcode == process.env.ALARM_PASSCODE) {
-    AfkBot.arm(function(err, success) {
-      if (err) { res.send(500) }
-      else {
-        res.render('alarm', { state: 'away' })
-      }
-    })
+  if (req.query.passcode) {
+    if (req.query.passcode == process.env.ALARM_PASSCODE) {
+      AfkBot.arm(function(err, success) {
+        if (err) { res.send(500) }
+        else {
+          res.render('alarm', { state: 'home' })
+        }
+      })
+    }
+    else {
+      res.send(504)
+    }
   }
-  else { res.send(500) }
+  else {
+    res.send(504)
+  }
 });
+
 
 router.get('/disarm', function(req, res) {
   if (req.query.passcode) {
