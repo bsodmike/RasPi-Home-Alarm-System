43bac2854162f8fd4435539f55def477b90a6ee8
Kevin Chandler <im.kevin@me.com>
2014-11-03 22:50:09 -0800

    rm legacy code

--- app.js
+++ app.js
@@ -32,13 +32,10 @@ var Alarm = require('./lib/alarm.js'),
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -54,58 +51,32 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 
 sensor.on('rise', function () {
   // check if we're home or away
-  // check if we should alert if away
-  // alert and create log if so
-  if (state == 'away') {
-    AfkBot.shouldWeAlert(function(err, response) {
-      try {
-        if (response == true) {
-          AfkBot.alert(function(err, success) {
-            if (err) { throw new Error(err) }
-            else {
-              AfkBot.createAlarmLog(function(err, success) {
-                if (err) { throw new Error(err) }
-                else {
-                  console.log('alerted successfully');
-                }
-              });
-            }
-          });
-        }
-        else { console.log('too soon to alert'); }
+  // if away: check if we should alert
+    // if yes: alert and create log
+  AfkBot.shouldWeAlert(function(err, response) {
+    try {
+      if (response == true) {
+        AfkBot.alert(function(err, success) {
+          if (err) { throw new Error(err) }
+          else {
+            AfkBot.createAlarmLog(function(err, success) {
+              if (err) { throw new Error(err) }
+              else {
+                console.log('alerted successfully');
+              }
+            });
+          }
+        });
       }
-      catch (e) {
-        if (e) { console.log(e); }
-      }
-    });
-  }
+      else { console.log('too soon to alert'); }
+    }
+    catch (e) {
+      if (e) { console.log(e); }
+    }
+  });
 })
 
 
