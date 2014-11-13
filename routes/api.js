1915208665c58ece8e4003aceb0ba5f79eadd3a3
Kevin Chandler <im.kevin@me.com>
2014-11-12 21:55:06 -0800

    fix instance of not arming/disarming; clean up

--- routes/api.js
+++ routes/api.js
@@ -5,41 +5,26 @@ var dotenv = require('dotenv').load();
 var AfkBot = require('../lib/afkbot.js');
 
 router.get('/arm', function(req, res) {
-  if (req.query.passcode) {
-    if (req.query.passcode == process.env.ALARM_PASSCODE) {
-      AfkBot.changeState({ type: 'arm' }, function(err, success) {
-        if (err) { res.send(500) }
-        else {
-          res.render('alarm', { status: 'home' })
-        }
-      })
-    }
-    else {
-      res.send(504)
-    }
-  }
-  else {
-    res.send(504)
-  }
+  if (req.query.passcode && req.query.passcode == process.env.ALARM_PASSCODE) {
+    AfkBot.changeState({ type: 'arm' },function(err, success) {
+      if (err) { res.send(500) }
+      else {
+        res.render('alarm', { status: 'away' })
+      }
+    })
+  else { res.send(504) }
 });
 
 router.get('/disarm', function(req, res) {
-  if (req.query.passcode) {
-    if (req.query.passcode == process.env.ALARM_PASSCODE) {
-      AfkBot.changeState({ type: 'disarm' },function(err, success) {
-        if (err) { res.send(500) }
-        else {
-          res.render('alarm', { status: 'home' })
-        }
-      })
-    }
-    else {
-      res.send(504)
-    }
-  }
-  else {
-    res.send(504)
-  }
+  if (req.query.passcode && req.query.passcode == process.env.ALARM_PASSCODE) {
+    AfkBot.changeState({ type: 'disarm' },function(err, success) {
+      if (err) { res.send(500) }
+      else {
+        res.render('alarm', { status: 'home' })
+      }
+    })
+  else { res.send(504) }
 });
+
 
 module.exports = router;
